9debbcfe451a4b3aa33928b3d9f58db807741885
goDeni <den4ikept11@gmail.com>
2023-05-13 12:26:21 +0500

    It work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::{prelude::*};
+
+
+
+#[tokio::main]
+async fn main() {
+    dotenv::from_filename(".env").unwrap();
+
+    pretty_env_logger::init();
+    log::info!("Starting bot...");
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    }).await;
 }
